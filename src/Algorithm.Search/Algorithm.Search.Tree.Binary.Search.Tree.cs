9256c16dfd9d01d9b4917218e56b98c8eb87779b
taesoku <taesoku@hotmail.com>
2018-05-09 08:41:37 +1000

    - Algorithm Search Tree Binary Search Tree : added

---
+++ src/Algorithm.Search/Algorithm.Search.Tree.Binary.Search.Tree.cs
@@ -0,0 +1,46 @@
+﻿
+namespace AlgorithmSearch
+{
+    class AlgorithmSearchTreeBinarySearchTree
+    {
+
+        public void Inorder(TreeNode node)
+        {
+            if (node == null) return;
+            Inorder(node.Left);
+            //print
+            Inorder(node.Right);
+        }
+
+        public void Preorder(TreeNode node)
+        {
+            if (node == null) return;
+            //print
+            Preorder(node.Left);
+            Preorder(node.Right);
+        }
+
+        public void Postorder(TreeNode node)
+        {
+            if (node == null) return;
+            Preorder(node.Left);
+            Preorder(node.Right);
+            //print
+        }
+
+    }
+
+    public class TreeNode
+    {
+        public int Data;
+        public TreeNode Left;
+        public TreeNode Right;
+
+    }
+
+
+
+    // inorder left root right
+    // preorder root left right
+    // postorder left right root
+}
